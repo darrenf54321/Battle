b7c1fb029e1cc2bda63299fb7b605ed0fae18478
crispinandrews <crispin.andrews@gmail.com>
2016-10-11 14:35:19 +0100

    stage 20 done

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,3 +9,14 @@ feature 'Enter names' do
     expect(page).to have_content 'Darren vs. Crispin'
   end
 end
+
+feature 'View player hit points' do
+  scenario 'see other players hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Darren'
+    fill_in :player_2_name, with: 'Crispin'
+    click_button 'Submit'
+    expect(page).to have_content 'Darren vs. Crispin'
+    expect(page).to have_content 'Darren: 5HP : Crispin: 2HP'
+  end
+end
