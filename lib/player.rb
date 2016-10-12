d67474a806938977906e5f82c24aa957c9af3048
Darrenf <darrenf54321@gmail.com>
2016-10-12 10:46:18 +0100

    The beginning of a brave new day with Lawrence

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,20 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hit_points
+
+  STARTING_HITPOINTS = 60
 
   def initialize(name)
     @name = name
+    @hit_points = STARTING_HITPOINTS
+  end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hitpoints -= 10
   end
 
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
   visit('/')
   fill_in :player_1_name, with: 'Darren'
-  fill_in :player_2_name, with: 'Crispin'
+  fill_in :player_2_name, with: 'Lawrence'
   click_button 'Submit'
 end
